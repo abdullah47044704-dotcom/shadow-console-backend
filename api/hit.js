25de67752bf95d4fab0108824953032ad30c5fa5
abdullah47044704-dotcom <abdullah47044704@gmail.com>
2026-02-21 15:53:15 +0600

    Update hit.js

--- api/hit.js
+++ api/hit.js
@@ -1,6 +1,27 @@
 export default async function handler(req,res){
   const { number } = req.query;
   if(!number) return res.status(400).json({error:"number required"});
-  await new Promise(r=>setTimeout(r,120));
-  res.json({ok:true});
+
+  // 🔽 এখানে তুমি API যোগ করবে
+  const APIS = [
+    https://smsboom.vercel.app/send-otp,
+    https://smsbooma.vercel.app/verify-phone,
+    https://smsboomc.vercel.app/shikho-send-otp,
+    https://smsboomg.vercel.app/bondi-login,
+    https://smsboomi.vercel.app/bioscope-login,
+    https://smsboomj.vercel.app/send-otp,
+    https://smsboomk.vercel.app/send-otp.
+    https://smsboomm.vercel.app/send-otp
+  
+  ];
+
+  for(const api of APIS){
+    try{
+      await fetch(`${api}?number=${encodeURIComponent(number)}`);
+    }catch(e){
+      // ignore error
+    }
+  }
+
+  res.json({ok:true, total: APIS.length});
 }
