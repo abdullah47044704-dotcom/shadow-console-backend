9dcdd36a8c59dd3f90b3c2fd45be5c0680c5b63c
abdullah47044704-dotcom <abdullah47044704@gmail.com>
2026-02-21 15:37:49 +0600

    Delete api directory

--- api/hit.js
+++
@@ -1,6 +0,0 @@
-export default async function handler(req,res){
-  const { number } = req.query;
-  if(!number) return res.status(400).json({error:"number required"});
-  await new Promise(r=>setTimeout(r,120));
-  res.json({ok:true});
-}
