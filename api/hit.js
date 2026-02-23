930787e91a5cf26a13dbd61182586980e7801a93
abdullah47044704-dotcom <abdullah47044704@gmail.com>
2026-02-24 00:43:13 +0600

    Add files via upload

--- api/hit.js
+++ api/hit.js
@@ -14,6 +14,8 @@ export default async function handler(req, res) {
     "https://smsboomj.vercel.app/send-otp",
     "https://smsboomk.vercel.app/send-otp",
     "https://smsboomm.vercel.app/send-otp",
+
+    "https://api3.example.com/verify"
   ];
 
   for (const api of APIS) {
