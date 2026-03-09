3a8cce38e9e76cc923b6fedbdd4d331b27924e58
abdullah47044704-dotcom <abdullah47044704@gmail.com>
2026-03-10 02:21:08 +0600

    Update login.js

--- api/login.js
+++ api/login.js
@@ -2,7 +2,7 @@ export default function handler(req,res){
   const {id,pass}=req.query;
   const LICENSES=[
     {id:"blueanzo حكر",pass:"463910",exp:"2027-03-01"},
-    {id:"1",pass:"1",exp:"2027-03-26"},
+    {id:"free",pass:"1",exp:"2026-03-11"},
     {id:"fahim",pass:"056810",exp:"2027-03-26"},
     {id:"client1",pass:"client123",exp:"2026-12-31"}
   ];
