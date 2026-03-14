759661f68da83ef69a7dc115f1db9e2e77f480d8
abdullah47044704-dotcom <abdullah47044704@gmail.com>
2026-03-14 16:53:24 +0600

    Update users.js

--- api/users.js
+++ api/users.js
@@ -1,6 +1,6 @@
 export const USERS = [
  { id:"blueanzo حكر", status:true },
- { id:"1", status:true },
+ { id:"1", status:false },
  { id:"fahim", status:true },
  { id:"client1", status:false }
 ];
