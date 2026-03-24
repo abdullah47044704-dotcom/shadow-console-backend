3e511e1048d58b55f80f64c980b304da11ca4e7e
abdullah47044704-dotcom <abdullah47044704@gmail.com>
2026-03-24 20:51:00 +0600

    Update users.js

--- api/users.js
+++ api/users.js
@@ -2,6 +2,7 @@ export const USERS = [
  { id:"blueanzo حكر", status:true, limit:200 },
  { id:"1", status:false, limit:200 },
  { id:"Hasib", status:true, limit:50 },
+ { id:"me", status:true, limit:50000 },
  { id:"fahim", status:true, limit:200 },
  { id:"mahabubremon925", status:true, limit:200 },
  { id:"client1", status:false, limit:200 }
