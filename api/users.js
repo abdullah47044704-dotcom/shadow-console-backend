b8807838065d3c25ebb1da0bb2d3eef98d7caa1f
abdullah47044704-dotcom <abdullah47044704@gmail.com>
2026-05-09 00:21:21 +0600

    Update user ID and limit for user 1

--- api/users.js
+++ api/users.js
@@ -1,6 +1,6 @@
 export const USERS = [
  { id:"blueanzo حكر", status:true, limit:200 },
- { id:"1", status:false, limit:200 },
+ { id:"4704", status:true, limit:2000000000 },
  { id:"Hasib", status:true, limit:50 },
  { id:"me", status:true, limit:50000 },
  { id:"fahim", status:true, limit:200 },
