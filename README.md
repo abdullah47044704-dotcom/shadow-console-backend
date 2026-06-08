1108e31ebb5eb40e753f070533a93ac89d3e1e7d
abdullah47044704-dotcom <abdullah47044704@gmail.com>
2026-06-08 13:55:52 +0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# shadow-console-backend
---
+++ api/users.js
@@ -0,0 +1,9 @@
+export const USERS = [
+ { id:"blueanzo حكر", status:true, limit:200 },
+ { id:"4704", status:true, limit:20000 },
+ { id:"Hasib", status:true, limit:50 },
+ { id:"me", status:true, limit:50000 },
+ { id:"fahim", status:true, limit:200 },
+ { id:"mahabubremon925", status:true, limit:200 },
+ { id:"client1", status:false, limit:200 }
+];
