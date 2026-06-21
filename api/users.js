af19a8a20d5f49e64bb00cb8d7307a73589d6c35
jocelyn360 <sarafrahman017@gmail.com>
2026-06-22 01:50:57 +0600

    Update users.js

--- api/users.js
+++ api/users.js
@@ -1,5 +1,5 @@
 export const USERS = [
- { id:"blueanzo حكر", status:true, limit:200 },
+ { id:"12", status:true, limit:200 },
  { id:"4704", status:true, limit:20000 },
  { id:"Hasib", status:true, limit:50 },
  { id:"me", status:true, limit:50000 },
